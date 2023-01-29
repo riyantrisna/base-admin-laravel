b1f80c26459d57516770ce44997436d17279e7b1
Riyan Trisna Wibowo <riyantrisnawibowo@gmail.com>
2023-01-29 10:55:42 +0700

    update hak akses

--- resources/views/access-right/detail.blade.php
+++ resources/views/access-right/detail.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group row">
     <label for="inputName" class="col-sm-3 col-form-label font-weight-normal">{{ multi_lang('name') }} <span class="text-red">*</span></label>
-    <div class="col-sm-9">
+    <div class="col-sm-9 pt-2">
         <input type="hidden" id="menugroup_menu_id" value="{{ $detail->menugroup_menu_id }}"/>
     @foreach ($language_master as $language)
         @foreach ($detail_name as $name)
@@ -14,13 +14,13 @@
 </div>
 <div class="form-group row">
     <label for="inputHomeMenu" class="col-sm-3 col-form-label font-weight-normal">{{ multi_lang('home_menu') }} <span class="text-red">*</span></label>
-    <div class="col-sm-9">
+    <div class="col-sm-9 pt-2">
         {{ $detail->menuname_name }}
     </div>
 </div>
 <div class="form-group row">
     <label for="inputMenuAccess" class="col-sm-3 col-form-label font-weight-normal">{{ multi_lang('menu_access') }} <span class="text-red">*</span></label>
-    <div class="col-sm-9">
+    <div class="col-sm-9 pt-2">
         <div id="inputMenuAccess" class="row p-1 border" style="margin: 1px;">
             {{ echo_menu_access() }}
             <div class="p-1"></div>
